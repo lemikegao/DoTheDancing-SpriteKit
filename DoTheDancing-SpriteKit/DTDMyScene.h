7e9780bde7d04d6bb8c95c8d377956a1aaade61b
Michael Gao <mgao.ucla@gmail.com>
2013-12-07 23:10:59 -0800

    Initial Commit

---
+++ DoTheDancing-SpriteKit/DTDMyScene.h
@@ -0,0 +1,13 @@
+//
+//  DTDMyScene.h
+//  DoTheDancing-SpriteKit
+//
+
+//  Copyright (c) 2013 Chin and Cheeks LLC. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface DTDMyScene : SKScene
+
+@end
---
+++ DoTheDancing-SpriteKit/DTDViewController.h
@@ -0,0 +1,14 @@
+//
+//  DTDViewController.h
+//  DoTheDancing-SpriteKit
+//
+
+//  Copyright (c) 2013 Chin and Cheeks LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+
+@interface DTDViewController : UIViewController
+
+@end
