20b1f2d387758e846dd8f39b5cd51a9a98255042
Michael Gao <mgao.ucla@gmail.com>
2013-12-22 13:49:33 -0800

    Added device check macros
    Detect iphone 4, 5, and iPad

--- DoTheDancing-SpriteKit/DTDConstants.h
+++ DoTheDancing-SpriteKit/DTDConstants.h
@@ -11,5 +11,9 @@
 
 #define RGBA(r,g,b,a)				[SKColor colorWithRed:r/255.0f green:g/255.0f blue:b/255.0f alpha:a]
 #define RGB(r,g,b)					RGBA(r, g, b, 1.0f)
+#define IS_IPAD (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad)
+#define IS_IPHONE (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone)
+#define IS_IPHONE_5 (IS_IPHONE && [[UIScreen mainScreen] bounds].size.height == 568.0f)
+#define IS_IPHONE_4 (IS_IPHONE && [[UIScreen mainScreen] bounds].size.height == 480.0f)
 
 #endif
