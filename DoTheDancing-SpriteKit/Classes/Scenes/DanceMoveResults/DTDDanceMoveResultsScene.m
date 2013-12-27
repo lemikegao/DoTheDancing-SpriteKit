5bdcbec90343bcbe5c9f336f5064052851da0c3f
Michael Gao <mgao.ucla@gmail.com>
2013-12-26 23:55:27 -0800

    Completed results scene with detailed results

--- DoTheDancing-SpriteKit/Classes/Scenes/DanceMoveResults/DTDDanceMoveResultsScene.m
+++ DoTheDancing-SpriteKit/Classes/Scenes/DanceMoveResults/DTDDanceMoveResultsScene.m
@@ -21,6 +21,7 @@
 @property (nonatomic, strong) NSMutableArray *stepResultsArray;
 @property (nonatomic) NSUInteger lastClickedIndex;
 @property (nonatomic) BOOL isResultExpanded;
+@property (nonatomic, strong) NSMutableArray *expandButtons;
 
 @end
 
@@ -39,6 +40,7 @@
         self.backgroundColor = RGB(249, 185, 56);
         _results = results;
         _danceMove = [DTDGameManager sharedGameManager].individualDanceMove;
+        _expandButtons = [[NSMutableArray alloc] initWithCapacity:self.danceMove.numIndividualIterations];
         
         [self _displayTopBar];
         [self _displayResults];
@@ -135,6 +137,44 @@
         result.position = CGPointMake(bg.size.width * 0.3, 0);
         [bg addChild:result];
         
+        // Add + button
+        SKButton *expandResultsButton = [SKButton buttonWithImageNamedNormal:@"results-plus" selected:@"results-plus-highlight"];
+        expandResultsButton.name = @(i).stringValue;
+        [expandResultsButton setTouchUpInsideTarget:self action:@selector(_pressedExpandResults:)];
+        expandResultsButton.position = CGPointMake(self.size.width * 0.87, bg.position.y);
+        [self addChild:expandResultsButton];
+        self.expandButtons[i] = expandResultsButton;
+        
+        // Add detailed step results
+        SKSpriteNode *detailedResultsBg = [SKSpriteNode spriteNodeWithColor:RGBA(249, 228, 172, 0.4) size:bg.size];
+        detailedResultsBg.position = CGPointMake(bg.position.x, bg.position.y - bg.size.height);
+        detailedResultsBg.hidden = YES;
+        [self addChild:detailedResultsBg];
+        
+        // Add 'Steps' label
+        SKLabelNode *stepsLabel = [SKLabelNode labelNodeWithFontNamed:@"Economica-Bold"];
+        stepsLabel.fontSize = 18;
+        stepsLabel.fontColor = RGB(56, 56, 56);
+        stepsLabel.horizontalAlignmentMode = SKLabelHorizontalAlignmentModeLeft;
+        stepsLabel.verticalAlignmentMode = SKLabelVerticalAlignmentModeCenter;
+        stepsLabel.text = @"Steps";
+        stepsLabel.position = CGPointMake(-detailedResultsBg.size.width * 0.43, detailedResultsBg.size.height * 0.15);
+        [detailedResultsBg addChild:stepsLabel];
+        
+        // Add detailed results circles
+        for (NSUInteger j=0; j<currentIterationResults.count; j++)
+        {
+            SKSpriteNode *dot = [SKSpriteNode spriteNodeWithImageNamed:@"results-dot"];
+            dot.color = [currentIterationResults[j] boolValue] == YES ? RGB(154, 140, 41) : RGB(153, 64, 32);
+            dot.colorBlendFactor = 1;
+            dot.position = CGPointMake(-detailedResultsBg.size.width * 0.4 + j*detailedResultsBg.size.width * 0.15, -detailedResultsBg.size.height * 0.20);
+            [detailedResultsBg addChild:dot];
+        }
+        
+        // Add result views to array
+        self.moveResultsArray[i] = bg;
+        self.stepResultsArray[i] = detailedResultsBg;
+        
         // Update positionY
         positionY = positionY - bg.size.height * 1.2;
     }
@@ -186,6 +226,81 @@
 - (void)_pressedTryAgain:(id)sender
 {
     [self.view presentScene:[DTDDanceMoveInstructionsScene sceneWithSize:self.size] transition:[SKTransition pushWithDirection:SKTransitionDirectionRight duration:0.25]];
+}
+
+- (void)_pressedExpandResults:(id)sender
+{
+    SKSpriteNode *tempSteps;
+    SKSpriteNode *tempMove;
+    SKButton *tempButton;
+    SKButton *tempSender = (SKButton *)sender;
+    NSUInteger senderIndex = tempSender.name.integerValue;
+    
+    // Nothing is currently expanded
+    if (self.isResultExpanded == NO)
+    {
+        [self _showDetailedResultsFromSender:sender];
+    }
+    else
+    {
+        // Switch previous clicked button to +
+        tempButton = self.expandButtons[self.lastClickedIndex];
+        tempButton.normalTexture = [SKTexture textureWithImageNamed:@"results-plus"];
+        tempButton.selectedTexture = [SKTexture textureWithImageNamed:@"results-plus-highlight"];
+        
+        // Hide expanded results
+        tempSteps = self.stepResultsArray[self.lastClickedIndex];
+        tempSteps.hidden = YES;
+        
+        // Move results up
+        for (NSUInteger i=self.lastClickedIndex+1; i<self.danceMove.numIndividualIterations; i++)
+        {
+            tempMove = self.moveResultsArray[i];
+            tempMove.position = CGPointMake(tempMove.position.x, tempMove.position.y + tempMove.size.height);
+            tempButton = self.expandButtons[i];
+            tempButton.position = CGPointMake(tempButton.position.x, tempMove.position.y);
+        }
+        
+        // If clicking previously expanded result, only move appropriate results up and don't show any steps
+        if (self.lastClickedIndex == senderIndex)
+        {
+            self.isResultExpanded = NO;
+        }
+        else
+        {
+            // Else, move results down and show detailed steps
+            [self _showDetailedResultsFromSender:sender];
+        }
+    }
+}
+
+- (void)_showDetailedResultsFromSender:(id)sender
+{
+    SKSpriteNode *tempSteps;
+    SKSpriteNode *tempMove;
+    SKButton *tempButton;
+    SKButton *tempSender = (SKButton *)sender;
+    NSUInteger senderIndex = tempSender.name.integerValue;
+    
+    // Switch button to minus
+    tempSender.normalTexture = [SKTexture textureWithImageNamed:@"results-minus"];
+    tempSender.selectedTexture = [SKTexture textureWithImageNamed:@"results-minus-highlight"];
+    
+    // Move appropriate results down
+    for (NSUInteger i=senderIndex+1; i<self.danceMove.numIndividualIterations; i++)
+    {
+        tempMove = self.moveResultsArray[i];
+        tempMove.position = CGPointMake(tempMove.position.x, tempMove.position.y - tempMove.size.height);
+        tempButton = self.expandButtons[i];
+        tempButton.position = CGPointMake(tempButton.position.x, tempMove.position.y);
+    }
+    
+    // Show detailed results
+    tempSteps = self.stepResultsArray[senderIndex];
+    tempSteps.hidden = NO;
+    
+    self.isResultExpanded = YES;
+    self.lastClickedIndex = senderIndex;
 }
 
 @end
