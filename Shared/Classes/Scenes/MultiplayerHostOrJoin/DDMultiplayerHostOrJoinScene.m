1b2a99e5e1c1bd828d72b3e667c2fddf90998348
Michael Gao <mgao.ucla@gmail.com>
2014-02-23 02:43:11 -0800

    Add prompt to enter dancer nickname when joining a party

--- Shared/Classes/Scenes/MultiplayerHostOrJoin/DDMultiplayerHostOrJoinScene.m
+++ Shared/Classes/Scenes/MultiplayerHostOrJoin/DDMultiplayerHostOrJoinScene.m
@@ -17,6 +17,13 @@
 #import "DDEMainMenuScene.h"
 #endif
 
+@interface DDMultiplayerHostOrJoinScene() <UITextFieldDelegate>
+
+@property (nonatomic, strong) UITextField *textField;
+@property (nonatomic, strong) SKSpriteNode *hostOrJoinMenuBg;
+
+@end
+
 @implementation DDMultiplayerHostOrJoinScene
 
 - (id)initWithSize:(CGSize)size
@@ -74,22 +81,22 @@
 - (void)_displayMenu
 {
     // Menu - Bg
-    SKSpriteNode *menuBg = [SKSpriteNode spriteNodeWithColor:RGB(249, 228, 172) size:CGSizeMake(227*self.sizeMultiplier, 144*self.sizeMultiplier)];
-    menuBg.anchorPoint = CGPointMake(0.5, 1);
-    menuBg.position = CGPointMake(self.size.width * 0.5, self.size.height * 0.74);
-    [self addChild:menuBg];
+    self.hostOrJoinMenuBg = [SKSpriteNode spriteNodeWithColor:RGB(249, 228, 172) size:CGSizeMake(227*self.sizeMultiplier, 144*self.sizeMultiplier)];
+    self.hostOrJoinMenuBg.anchorPoint = CGPointMake(0.5, 1);
+    self.hostOrJoinMenuBg.position = CGPointMake(self.size.width * 0.5, self.size.height * 0.74);
+    [self addChild:self.hostOrJoinMenuBg];
     
     // Menu - Host button
     SKButton *hostButton = [SKButton buttonWithImageNamedNormal:@"multiplayer-hostorjoin-button-host" selected:@"multiplayer-hostorjoin-button-host-highlight"];
-    hostButton.position = CGPointMake(0, -menuBg.size.height * 0.27);
+    hostButton.position = CGPointMake(0, -self.hostOrJoinMenuBg.size.height * 0.27);
     [hostButton setTouchUpInsideTarget:self action:@selector(_pressedHostButton:)];
-    [menuBg addChild:hostButton];
+    [self.hostOrJoinMenuBg addChild:hostButton];
     
     // Menu - Join button
     SKButton *joinButton = [SKButton buttonWithImageNamedNormal:@"multiplayer-hostorjoin-button-join" selected:@"multiplayer-hostorjoin-button-join-highlight"];
-    joinButton.position = CGPointMake(0, -menuBg.size.height * 0.73);
+    joinButton.position = CGPointMake(0, -self.hostOrJoinMenuBg.size.height * 0.73);
     [joinButton setTouchUpInsideTarget:self action:@selector(_pressedJoinButton:)];
-    [menuBg addChild:joinButton];
+    [self.hostOrJoinMenuBg addChild:joinButton];
     
     // Disable join button if connected to another device (like external screen)
     if ([DDGameManager sharedGameManager].sessionManager.isConnected)
@@ -107,6 +114,9 @@
 #pragma mark - Button actions
 - (void)_pressedBack:(id)sender
 {
+    // Remove UITextField
+    [self.textField removeFromSuperview];
+    
     // Segue back to ConnectedToExternal screen
     if ([DDGameManager sharedGameManager].sessionManager.isConnected)
     {
@@ -140,7 +150,73 @@
 
 - (void)_pressedJoinButton:(id)sender
 {
+    [self _showNicknamePrompt];
+}
+
+- (void)_pressedDanceButton:(id)sender
+{
     
+}
+
+#pragma mark - Nickname
+- (void)_showNicknamePrompt
+{
+    CGFloat menuBgPositionY = self.size.height * 0.74;
+#warning TODO - Do not show on external screen. So what then?
+    // Hide previous menu immediately & display fade layer
+    self.hostOrJoinMenuBg.hidden = YES;
+    SKSpriteNode *fadeLayer = [SKSpriteNode spriteNodeWithColor:RGBA(0, 0, 0, 0.5) size:self.size];
+    fadeLayer.anchorPoint = CGPointMake(0, 0);
+    [self addChild:fadeLayer];
+    
+    // Menu bg
+    SKSpriteNode *menuBg = [SKSpriteNode spriteNodeWithColor:RGB(249, 228, 172) size:CGSizeMake(227*self.sizeMultiplier, 144*self.sizeMultiplier)];
+    menuBg.anchorPoint = CGPointMake(0.5, 1);
+    menuBg.position = CGPointMake(self.size.width * 0.5, 0);
+    [fadeLayer addChild:menuBg];
+    
+    // Label - Dancer name?
+    SKLabelNode *nameLabel = [SKLabelNode labelNodeWithFontNamed:@"Economica-Bold"];
+    nameLabel.fontSize = 20;
+    nameLabel.text = @"What's your dancer name?";
+    nameLabel.fontColor = RGB(56, 56, 56);
+    nameLabel.position = CGPointMake(0, -menuBg.size.height * 0.15);
+    [menuBg addChild:nameLabel];
+    
+    // Sprite - Fake UITextField for SpriteKit
+    SKSpriteNode *fakeTextField = [SKSpriteNode spriteNodeWithColor:RGB(255, 251, 231) size:CGSizeMake(menuBg.size.width * 0.84, 32)];
+    fakeTextField.position = CGPointMake(0, -menuBg.size.height * 0.32);
+    [menuBg addChild:fakeTextField];
+    
+    // UITextField
+    self.textField = [[UITextField alloc] initWithFrame:CGRectMake(menuBg.position.x - menuBg.size.width * 0.42 , self.size.height - (menuBgPositionY - menuBg.size.height * 0.21), menuBg.size.width * 0.84, 32)];
+    self.textField.backgroundColor = RGB(255, 251, 231);
+    self.textField.textColor = RGB(219, 133, 20);
+    self.textField.font = [UIFont fontWithName:@"Economica-Bold" size:18];
+    self.textField.autocorrectionType = UITextAutocorrectionTypeNo;
+    self.textField.delegate = self;
+    // Add padding on left side
+    UIView *paddingView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 8, 20)];
+    paddingView.backgroundColor = self.textField.backgroundColor;
+    self.textField.leftView = paddingView;
+    self.textField.leftViewMode = UITextFieldViewModeAlways;
+    self.textField.hidden = YES;
+    [self.view addSubview:self.textField];
+    
+    // Button - Let's Dance!
+    SKButton *danceButton = [SKButton buttonWithImageNamedNormal:@"multiplayer-hostorjoin-button-lets-dance" selected:@"multiplayer-hostorjoin-button-lets-dance-highlight"];
+    danceButton.position = CGPointMake(0, -menuBg.size.height * 0.73);
+    [danceButton setTouchUpInsideTarget:self action:@selector(_pressedDanceButton:)];
+    [menuBg addChild:danceButton];
+    
+    // Animations! Move new menu up to previous menu position and focus on textfield
+    SKAction *moveAction = [SKAction moveTo:CGPointMake(self.size.width * 0.5, menuBgPositionY) duration:0.25];
+    SKAction *showTextFieldAction = [SKAction runBlock:^{
+        self.textField.hidden = NO;
+        [self.textField becomeFirstResponder];
+    }];
+    
+    [menuBg runAction:[SKAction sequence:@[moveAction, showTextFieldAction]]];
 }
 
 #pragma mark - Networking
